e5b2a5ddc7ae2b329cef78ab36a4f9aad78c2209
Geoff Moller <geoff.moller@gmail.com>
2019-05-21 14:51:18 -0700

    constantize test table names

--- src/test/java/query/PredicateBuilderFloatTest.java
+++ src/test/java/query/PredicateBuilderFloatTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.function.Predicate;
 
+import static db.Database.RATINGS;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -21,56 +22,56 @@ class PredicateBuilderFloatTest {
 
     @Test
     void testFloatEqualityTrue() {
-        Tuple t = new Tuple(db, "ratings", "1,2,3.5,1112486027");
+        Tuple t = new Tuple(db, RATINGS, "1,2,3.5,1112486027");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "rating = 3.5");
         assertTrue(p.test(t));
     }
 
     @Test
     void testFloatEqualityFalse() {
-        Tuple t = new Tuple(db, "ratings", "1,2,3.5,1112486027");
+        Tuple t = new Tuple(db, RATINGS, "1,2,3.5,1112486027");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "rating = 3.4");
         assertFalse(p.test(t));
     }
 
     @Test
     void testFloatInequalityTrue() {
-        Tuple t = new Tuple(db, "ratings", "1,2,3.5,1112486027");
+        Tuple t = new Tuple(db, RATINGS, "1,2,3.5,1112486027");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "rating != 3.4");
         assertTrue(p.test(t));
     }
 
     @Test
     void testFloatInequalityFalse() {
-        Tuple t = new Tuple(db, "ratings", "1,2,3.5,1112486027");
+        Tuple t = new Tuple(db, RATINGS, "1,2,3.5,1112486027");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "rating != 3.5");
         assertFalse(p.test(t));
     }
 
     @Test
     void testFloatLessThanTrue() {
-        Tuple t = new Tuple(db, "ratings", "1,2,3.5,1112486027");
+        Tuple t = new Tuple(db, RATINGS, "1,2,3.5,1112486027");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "rating < 3.6");
         assertTrue(p.test(t));
     }
 
     @Test
     void testFloatLessThanFalse() {
-        Tuple t = new Tuple(db, "ratings", "1,2,3.5,1112486027");
+        Tuple t = new Tuple(db, RATINGS, "1,2,3.5,1112486027");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "rating < 3.5");
         assertFalse(p.test(t));
     }
 
     @Test
     void testFloatGreaterThanTrue() {
-        Tuple t = new Tuple(db, "ratings", "1,2,3.5,1112486027");
+        Tuple t = new Tuple(db, RATINGS, "1,2,3.5,1112486027");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "rating > 3.4");
         assertTrue(p.test(t));
     }
 
     @Test
     void testFloatGreaterThanFalse() {
-        Tuple t = new Tuple(db, "ratings", "1,2,3.5,1112486027");
+        Tuple t = new Tuple(db, RATINGS, "1,2,3.5,1112486027");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "rating > 3.5");
         assertFalse(p.test(t));
     }
--- src/test/java/query/PredicateBuilderIntTest.java
+++ src/test/java/query/PredicateBuilderIntTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.function.Predicate;
 
+import static db.Database.LINKS;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PredicateBuilderIntTest {
@@ -21,56 +22,56 @@ class PredicateBuilderIntTest {
 
     @Test
     void testIntEqualityTrue() {
-        Tuple t = new Tuple(db, "links", "1,1,1");
+        Tuple t = new Tuple(db, LINKS, "1,1,1");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "movieId = 1");
         assertTrue(p.test(t));
     }
 
     @Test
     void testIntEqualityFalse() {
-        Tuple t = new Tuple(db, "links", "2,1,1");
+        Tuple t = new Tuple(db, LINKS, "2,1,1");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "movieId = 1");
         assertFalse(p.test(t));
     }
 
     @Test
     void testIntInequalityTrue() {
-        Tuple t = new Tuple(db, "links", "1,1,1");
+        Tuple t = new Tuple(db, LINKS, "1,1,1");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "movieId != 2");
         assertTrue(p.test(t));
     }
 
     @Test
     void testIntInequalityFalse() {
-        Tuple t = new Tuple(db, "links", "2,1,1");
+        Tuple t = new Tuple(db, LINKS, "2,1,1");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "movieId != 2");
         assertFalse(p.test(t));
     }
 
     @Test
     void testIntLessThanTrue() {
-        Tuple t = new Tuple(db, "links", "1,1,1");
+        Tuple t = new Tuple(db, LINKS, "1,1,1");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "movieId < 2");
         assertTrue(p.test(t));
     }
 
     @Test
     void testIntLessThanFalse() {
-        Tuple t = new Tuple(db, "links", "1,1,1");
+        Tuple t = new Tuple(db, LINKS, "1,1,1");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "movieId < 1");
         assertFalse(p.test(t));
     }
 
     @Test
     void testIntGreaterThanTrue() {
-        Tuple t = new Tuple(db, "links", "1,1,1");
+        Tuple t = new Tuple(db, LINKS, "1,1,1");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "movieId > 0");
         assertTrue(p.test(t));
     }
 
     @Test
     void testIntGreaterThanFalse() {
-        Tuple t = new Tuple(db, "links", "1,1,1");
+        Tuple t = new Tuple(db, LINKS, "1,1,1");
         Predicate<Tuple> p = new PredicateBuilder().build(t, "movieId > 1");
         assertFalse(p.test(t));
     }
