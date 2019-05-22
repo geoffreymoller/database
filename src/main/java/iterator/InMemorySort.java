579c4ade26e02feab12e055723290d6a83472aca
Geoff Moller <geoff.moller@gmail.com>
2019-05-22 11:01:28 -0700

    in-memory sort + test

---
+++ src/main/java/iterator/InMemorySort.java
@@ -0,0 +1,59 @@
+package iterator;
+
+import com.google.common.collect.Lists;
+import db.Schema;
+import entity.Tuple;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class InMemorySort implements Iterator {
+
+    private final String attribute;
+    private Iterator child;
+    private ArrayList<Tuple> all;
+    private java.util.Iterator<Tuple> iterator;
+
+    InMemorySort(String attribute, Iterator child, Schema schema) {
+        this.child = child;
+        this.attribute = attribute;
+        this.init();
+    }
+
+    @Override
+    public void init() {
+        this.child.init();
+
+        Tuple next = child.next();
+        this.all = Lists.newArrayList();
+        while (next != null) {
+            all.add(next);
+            next = child.next();
+        }
+        all.sort((t1, t2) -> {
+            Integer v1 = (Integer) t1.get(attribute);
+            Integer v2 = (Integer) t2.get(attribute);
+            if(Objects.equals(v1, v2)) {
+                return 0;
+            } else if(v1 < v2){
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+    }
+
+    public Tuple next() {
+        if (all.size() > 0) {
+            return all.remove(0);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+}
---
+++ src/test/java/iterator/InMemorySortTest.java
@@ -0,0 +1,62 @@
+package iterator;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Ordering;
+import db.Schema;
+import entity.Link;
+import entity.Tuple;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static db.Schema.IMDB_ID;
+import static db.Schema.LINKS;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InMemorySortTest {
+
+    private Schema schema;
+
+    @BeforeEach
+    void setUp() {
+        schema = new Schema("/Users/geoffreymoller/Code/database/src/test/resources/next/");
+    }
+
+    @Test
+    void init() {
+        ArrayList<Object> all = Lists.newArrayList();
+        FileScan fs = new FileScan(LINKS, schema);
+        Tuple next = fs.next();
+        while (next != null) {
+            all.add(next);
+            next = fs.next();
+        }
+        List<Integer> imdbIDs = all.stream().map(
+            link -> (Integer) ((Tuple) link).get(IMDB_ID)
+        ).collect(Collectors.toList());
+        boolean sorted = Ordering.natural().isOrdered(imdbIDs);
+        assertFalse(sorted);
+
+        all.clear();
+
+        InMemorySort sort = new InMemorySort(IMDB_ID, fs, schema);
+        next = sort.next();
+        while (next != null) {
+            all.add(next);
+            next = sort.next();
+        }
+        imdbIDs = all.stream().map(
+            link -> (Integer) ((Tuple) link).get(IMDB_ID)
+        ).collect(Collectors.toList());
+        sorted = Ordering.natural().isOrdered(imdbIDs);
+        assertTrue(sorted);
+    }
+
+    @Test
+    void next() {
+    }
+}
